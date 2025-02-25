4a083ab0000e58654b7ea6cfa6d8359cb0273705
Valentina Denisenko <144355737+Dolce-Vitah@users.noreply.github.com>
2025-02-25 19:39:49 +0300

    Changed integration

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ app.whenReady().then(() => {
         width: 1200,
         height: 800,
         webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
+            nodeIntegration: true,
+            contextIsolation: false,
         },
     });
 
