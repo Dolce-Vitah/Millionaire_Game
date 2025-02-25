04fe06da55b380aec3651507b777a301a2dcd71e
Valentina Denisenko <144355737+Dolce-Vitah@users.noreply.github.com>
2025-02-26 01:34:11 +0300

    Added wait-on

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const waitOn = require("wait-on");
+
+const CLIENT_URL = "http://localhost:3000";
 
 let mainWindow;
 
 app.whenReady().then(() => {
+    await waitOn({ resources: [CLIENT_URL], timeout: 10000 });
+
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
@@ -13,5 +18,5 @@ app.whenReady().then(() => {
         },
     });
 
-    mainWindow.loadURL("http://localhost:3000"); 
+    mainWindow.loadURL(CLIENT_URL); 
 });
